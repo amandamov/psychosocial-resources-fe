7389896cc7c111e93996a9483877c8ccb2febfd1
nunoluis85 <83136872+nunoluis85@users.noreply.github.com>
2023-02-22 16:52:34 +0100

    adding footer

--- psychosocial-resources/src/App.js
+++ psychosocial-resources/src/App.js
@@ -9,6 +9,7 @@ import migra_mind from './img/migra_mind.png';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigationbar from './components/Navigationbar';
 import RenderEventCard from './components/RenderEventCard';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       < Navigationbar />
       {/* <Test /> */}
       <RenderEventCard />
+      <Footer />
   </div>
   );
 }
