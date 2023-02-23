b8459cc45be20b26606c14c2ab09012b57b53218
nunoluis85 <83136872+nunoluis85@users.noreply.github.com>
2023-02-23 20:20:30 +0100

    footer new

--- psychosocial-resources/src/routes/layout/Layout.js
+++ psychosocial-resources/src/routes/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
+import Footer from "../../components/Footer";
 import Navigationbar from "../../components/Navigationbar";
 
 const Layout = () => {
@@ -7,6 +8,7 @@ const Layout = () => {
     <>
       <Navigationbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
