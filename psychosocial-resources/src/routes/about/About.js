8a3110f0435d32aa8549aa47ebe60d493b3bc7e0
Amanda Maria <amanda.moov@gmail.com>
2023-03-06 22:31:47 +0100

    adding text on about us

--- psychosocial-resources/src/routes/about/About.js
+++ psychosocial-resources/src/routes/about/About.js
@@ -3,10 +3,20 @@ import './About.css'
 
 const About = () => {
     return (
-        // you can delete this image:
-        <div className="img-migracode">
-            <img src={Migracode}/>
+        <div className="container-about">
+            <div className="migracode">
+                <img src={Migracode}/>
+                <h3>
+                    We are web developers trained by <b>Migracode</b> <br></br>
+                    and Migramind is our final project where we are applying everything we learned over 7 months.<br></br> 
+                    We are immigrants living in Barcelona and our team is made up of people from Brazil, Cape Verde, Colombia and Venezuela.
+                </h3>
+            </div>
+            <div className="team">
+                
+            </div>
         </div>
+
     )
 };
 
